3d4930837e576b4df6771426007e7baa023d00f4
Wade Dominic <wade@lambdaforge.io>
2020-12-24 14:09:00 +0100

    Minimal setup

---
+++ src/frontend/db.cljs
@@ -0,0 +1,71 @@
+(ns frontend.db
+  (:require [datahike.api :as d]
+            [datahike.impl.entity :as de]
+            [clojure.core.async :as async :refer [go <!]]))
+
+  (def schema [{:db/ident       :name
+                :db/cardinality :db.cardinality/one
+                :db/index       true
+                :db/unique      :db.unique/identity
+                :db/valueType   :db.type/string}
+               {:db/ident       :sibling
+                :db/cardinality :db.cardinality/many
+                :db/valueType   :db.type/ref}
+               {:db/ident       :age
+                :db/cardinality :db.cardinality/one
+                :db/valueType   :db.type/number}
+               {:db/ident       :friend
+                :db/cardinality :db.cardinality/many
+                :db/valueType :db.type/ref}])
+
+
+(def cfg-idb {:store  {:backend :indexeddb :id "idb-sandbox"}
+              :keep-history? false
+              :schema-flexibility :write
+              :initial-tx schema})
+
+(comment
+  ;; REPL-driven code
+
+
+   ;; Create an indexeddb store.
+  (d/create-database cfg-idb)
+
+  ;; Connect to the indexeddb store.
+  (go (def conn-idb (<! (d/connect cfg-idb))))
+
+    ;; Transact some data to the store.
+  (d/transact conn-idb [{:name "Alice"
+                         :age  26}
+                        {:name "Bob"
+                         :age  35
+                         :_friend [{:name "Mike"
+                                    :age 28}]}
+                        {:name  "Charlie"
+                         :age   45
+                         :sibling [[:name "Alice"] [:name "Bob"]]}])
+
+  ;; Run a query against the store.
+  (go (println (<! (d/q '[:find ?e ?a ?v ?t
+                          :in $ ?a
+                          :where [?e :name ?v ?t] [?e :age ?a]]
+                        @conn-idb
+                        35))))
+
+    ;; Use the Entity API for "Bob".
+  (go
+    (let [e (<! (d/entity @conn-idb 6))]
+      (println (<! (e :name)))))
+
+  ;; Release the connection from the store.
+  ;; This is necessary for deletion.
+  (d/release conn-idb)
+
+  ;; Delete the store. 
+  ;; This can be done immediately after creation.
+  ;; In a fresh browser session or after releasing the connection. 
+  (d/delete-database cfg-idb)
+  
+  
+  ;; Format blocker
+  )
